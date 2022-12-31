ed8658f03548fb732cc4997212bc1f02ad926c85
Marcel Pichler <marcel.pichler@outlook.at>
2022-12-31 23:35:09 +0100

    add additional days

---
+++ day18/solve.py
@@ -0,0 +1,111 @@
+import itertools
+
+from utils.aoc_base import Day
+import numpy as np
+
+
+class PartA(Day):
+
+    def parse(self, text: str, data):
+        lines = text.splitlines()
+
+        cubes = np.zeros((len(lines), 3), dtype=int)
+        cubes = []
+        for i, line in enumerate(lines):
+            cube = []
+            for j, v in enumerate(line.strip().split(',')):
+                #cubes[i, j] = int(v)
+                cube.append(int(v))
+
+            cubes.append(cube)
+
+        data.cubes = cubes
+
+    def compute2(self, data):
+        touching = 0
+        seen = []
+
+        for i in range(len(data.cubes)):
+            for cube2 in seen:
+                if self.is_touching(data.cubes[i], cube2):
+                    touching += 1
+
+            seen.append(data.cubes[i])
+
+        return len(data.cubes) * 6 - touching * 2
+
+    @staticmethod
+    def is_touching(cube, cube2):
+        norm = abs(cube[0] - cube2[0]) + abs(cube[1] - cube2[1]) + abs(cube[2] - cube2[2])
+        return norm == 1
+
+    def compute(self, data):
+        touching = 0
+
+        for cube, cube2 in itertools.combinations(data.cubes, 2):
+            if cube == cube2:
+                continue
+
+            if self.is_touching(cube, cube2):
+                touching += 1
+
+        return len(data.cubes) * 6 - touching * 2
+
+    def example_answer(self):
+        return 64
+
+class PartB(PartA):
+    def config(self, data):
+        min_bound = [1000, 1000, 1000]
+        max_bound = [-1000, -1000, -1000]
+
+        for cube in data.cubes:
+            for i in range(3):
+                min_bound[i] = min(min_bound[i], cube[i]-1)
+                max_bound[i] = max(max_bound[i], cube[i]+1)
+
+        data.min_bound = min_bound
+        data.max_bound = max_bound
+
+    def compute(self, data):
+        touching = 0
+        seen = []
+
+        exterior = []
+        todo = [data.min_bound]
+        while len(todo) > 0:
+            cube = todo.pop(0)
+
+            if cube in exterior:
+                continue
+
+            if cube in data.cubes:
+                continue
+
+            if cube[0] < data.min_bound[0] or cube[1] < data.min_bound[1] or cube[2] < data.min_bound[2]:
+                continue
+            if cube[0] > data.max_bound[0] or cube[1] > data.max_bound[1] or cube[2] > data.max_bound[2]:
+                continue
+
+            exterior.append(cube)
+
+            todo.append([cube[0]+1, cube[1], cube[2]])
+            todo.append([cube[0], cube[1]+1, cube[2]])
+            todo.append([cube[0], cube[1], cube[2]+1])
+            todo.append([cube[0]-1, cube[1], cube[2]])
+            todo.append([cube[0], cube[1]-1, cube[2]])
+            todo.append([cube[0], cube[1], cube[2]-1])
+
+        for i in range(len(data.cubes)):
+            for cube2 in exterior:
+                if self.is_touching(data.cubes[i], cube2):
+                    touching += 1
+
+            seen.append(data.cubes[i])
+
+        return touching
+
+    def example_answer(self):
+        return 58
+
+Day.do_day(18, 2022, PartA, PartB)
---
+++ day19/solve.py
@@ -0,0 +1,105 @@
+import re
+import numpy as np
+import nographs
+
+from utils.aoc_base import Day
+
+
+class Blueprint:
+    def __init__(self, id, costs):
+        # ore, clay, obsidian, geode
+        self.id = id
+        self.costs = costs
+
+    @staticmethod
+    def from_string(str):
+        match = re.match(r'Blueprint (\d+): Each ore robot costs (\d+) ore\. Each clay robot costs (\d+) ore\. Each obsidian robot costs (\d+) ore and (\d+) clay\. Each geode robot costs (\d+) ore and (\d+) obsidian\.', str)
+        id = int(match.group(1))
+        ore_robot_cost = int(match.group(2))
+        clay_robot_cost = int(match.group(3))
+        obsidian_robot_ore_cost = int(match.group(4))
+        obsidian_robot_clay_cost = int(match.group(5))
+        geode_robot_ore_cost = int(match.group(6))
+        geode_robot_obsidian_cost = int(match.group(7))
+        costs = [(ore_robot_cost, 0, 0, 0),
+                 (clay_robot_cost, 0, 0, 0),
+                 (obsidian_robot_ore_cost, obsidian_robot_clay_cost, 0, 0),
+                 (geode_robot_ore_cost, 0, geode_robot_obsidian_cost, 0)]
+
+        return Blueprint(id, costs)
+
+    def produce(self, minutes):
+        def next_edges(state, traversal):
+            materials, producing = state
+            minutes_remaining = minutes - traversal.depth
+            if minutes_remaining == 1:
+                return
+
+            new_materials = tuple(sum(x) for x in zip(materials, producing))
+            robots_build = 0
+            for robot_id in range(4):
+                if robot_id < 3 and producing[robot_id] >= max_robots[robot_id]:
+                    continue
+                if all(cost <= material for cost, material in zip(self.costs[robot_id], materials)):
+                    robots_build += 1
+                    yield (tuple(material - cost for material, cost in zip(new_materials, self.costs[robot_id])), tuple(x + 1 if i == robot_id else x for i, x in enumerate(producing))), 0 if robot_id == 3 else minutes_remaining
+
+            if robots_build == 4:
+                return
+
+            max_materials = tuple(material + production * minutes_remaining for material, production in zip(new_materials, producing))
+            robots_buildable = 0
+            for robot_id in range(4):
+                if all(cost <= material for cost, material in zip(self.costs[robot_id], max_materials)):
+                    robots_buildable += 1
+
+            if robots_buildable == 0:
+                return
+
+            yield (new_materials, producing), minutes_remaining
+
+        max_robots = tuple([max(self.costs[r][m] for r in range(4)) for m in range(4)])
+        traversal = nographs.TraversalShortestPaths(next_edges)
+        for state in traversal.start_from(((0, 0, 0, 0), (1, 0, 0, 0))):
+            materials, produce = state
+            if traversal.depth == minutes - 1:
+                return materials[-1] + produce[-1]
+
+        raise RuntimeError('no solution found')
+
+
+class PartA(Day):
+
+    def parse(self, text: str, data):
+        lines = text.splitlines()
+        if lines[0] == 'Blueprint 1:':
+            lines = ['Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.',
+                     'Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.']
+        data.blueprints = [Blueprint.from_string(str) for str in lines]
+
+    def compute(self, data):
+        quality_levels = []
+        for blueprint in data.blueprints:
+            quality_levels.append(blueprint.produce(24) * blueprint.id)
+
+        return sum(quality_levels)
+
+    def example_answer(self):
+        return 33
+
+
+class PartB(PartA):
+
+    def compute(self, data):
+        geodes = []
+        for i in range(min(3, len(data.blueprints))):
+            blueprint = data.blueprints[i]
+            geodes.append(blueprint.produce(32))
+
+        return int(np.product(geodes))
+
+    def example_answer(self):
+        return 3472
+
+
+Day.do_day(19, 2022, PartA, PartB)
